8132654348ec16a5d4bce58390aa4913e773acd6
abhinavshiv7 <abhinav.shiv7@gmail.com>
2026-07-30 01:33:07 +0530

    chore: remove tracked backend/dist directory, delete empty docker folder, and ignore dist in .gitignore

--- backend/dist/routes/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authController_1 = require("../controllers/authController");
-const router = (0, express_1.Router)();
-router.post('/google', authController_1.googleLogin);
-router.post('/login-email', authController_1.loginEmail);
-router.post('/register-email', authController_1.registerEmail);
-exports.default = router;
